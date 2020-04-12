a75e53f9e72433fada23fcf8b3f691d415031684
DMKFlex <daymon9991@gmail.com>
2020-04-12 15:58:11 +0300

    upd1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst();
+        System.out.println(i);
+        String s = pair.getSecond();
+        System.out.println(s);
+        Pair<Integer,String> pair1=Pair.of(1,"hello");
+        boolean mstbtrue = pair.equals(pair1);
+        System.out.println(mstbtrue);
+        boolean mstbtrue2=pair.hashCode()==pair1.hashCode();
+        System.out.println(mstbtrue2);
+    }
 }
--- src/Pair.java
+++ src/Pair.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+public class Pair<R, T> {
+    private R first;
+    private T second;
+    Pair pair;
 
-public class Pair {
+    private Pair(R first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public R getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Pair pair = (Pair) obj;
+        return first == pair.first && second == pair.second;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + first.hashCode();
+        result = prime * result + second.hashCode();
+
+        return result;
+    }
+    public static <T, R> Pair of(T first, R second){
+        return  new Pair(first, second);
+    }
 }
+
